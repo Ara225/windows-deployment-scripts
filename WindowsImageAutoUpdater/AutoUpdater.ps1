64d64d5054fea550b855ad4524afcd0f54036683
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-12-14 22:42:14 +0000

    updates

--- WindowsImageAutoUpdater/AutoUpdater.ps1
+++ WindowsImageAutoUpdater/AutoUpdater.ps1
@@ -1,22 +1,44 @@
+function logger{
+    param (
+        $TextToLog
+    )
+    Tee-Object -InputObject $TextToLog -FilePath $ScriptLocation\ImageUpdater.log -Append
+}
+
 $WIM_FILES_FOLDER = "C:\Users\User\Documents\WimsToBeUpdated"
 $ScriptLocation = Get-Location
 $ScriptLocation = $ScriptLocation.Path
 if (Test-Path $ScriptLocation\ImageUpdater1.log) {
+    logger -TextToLog "$(Get-Date) INFO: Removing ImageUpdater1.log\n"
     Remove-Item $ScriptLocation\ImageUpdater1.log
 }
 if (Test-Path $ScriptLocation\ImageUpdater.log) {
+    logger -TextToLog "$(Get-Date) INFO: Moving ImageUpdater.log to ImageUpdater1.log\n"
     Move-Item $ScriptLocation\ImageUpdater.log $ScriptLocation\ImageUpdater1.log
 }
-Install-Module -Name OSDBuilder 
-try {
-    $files = Get-ChildItem $WIM_FILES_FOLDER -File
-}
-catch {
-    Write-Output "Error happened while trying to get contents of the WIM_FILES_FOLDER" >>$ScriptLocation\ImageUpdater.log
-    throw
-}
-for ($i = 0; $i -lt $files.Count; $i++) {
-    if ($files[$i].Extension.ToLower() -match "wim" -or $files[$i].Extension.ToLower() -match "esd") {
-        Write-Host $files[$i]
+
+logger -TextToLog "$(Get-Date) INFO: Installing OSDBuilder\n" 
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module -Name OSDBuilder -Scope CurrentUser -Confirm:$false
+logger -TextToLog "$(Get-Date) INFO: Getting contents of the WIM_FILES_FOLDER\n" 
+$files = Get-ChildItem $WIM_FILES_FOLDER -File
+
+for ($file = 0; $file -lt $files.Count; $file++) {
+    try {
+        if ($files[$file].Extension.ToLower() -match "wim" -or $files[$file].Extension.ToLower() -match "esd") {
+            $FileName = $files[$file].FullName
+            logger -TextToLog "$(Get-Date) INFO: Getting image info from $FileName \n" 
+            $filemageContents = Get-WindowsImage -ImagePath $FileName
+            for ($ImageIndex = 0; $ImageIndex -lt $filemageContents.Count; $ImageIndex++) {
+                logger -TextToLog "$(Get-Date) INFO: Importing $FileName image index $ImageIndex \n"
+                Import-OSMedia -Path $FileName -ImageIndex $filemageContents[$ImageIndex].ImageIndex
+            }
+        }
+    }
+    catch {
+        for ($i = 0; $i -lt $Error.Count; $i++) {
+            logger -TextToLog $Error[$i].Exception
+        }
+        continue
     }
 }
---
+++ WindowsImageAutoUpdater/README.md
@@ -0,0 +1,23 @@
+```JSON
+[
+    {
+        "ImagesFolderPath": "...",
+        "MasterImageISOPath": "...",
+        "MasterImageDisplayName": "...",
+        "ReportingURL": "",
+        "DerivateImages": [
+            {
+                "DisplayName": "...",
+                "WinGetDependencies": [
+                    "URL to a yaml in WinGet repo"
+                ],
+                "CommandsToBeRunInUnattend": "",
+                "UserName": "...",
+                "Password": "...",
+                "PasswordIsPlainText": false,
+                "WindowsEdition": "..."
+            }
+        ]
+    }
+]
+```
